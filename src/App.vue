38f6fd91e09c095265c19050eee3f3dc6f90bfb5
kaycn <liaomingzhan@gmail.com>
2020-03-04 10:06:51 +0800

    按需配置完成

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+      <a-button>这是按钮</a-button>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
